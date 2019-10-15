9da324384aafef6c1dc26510a9b3b0ec3304333f
Adrian Gordon <adriangordon60@gmail.com>
2019-10-15 12:12:35 +0100

    added dense layer to rezise encoder states

--- model.py
+++ model.py
@@ -29,6 +29,15 @@ class SeqToSeqModel:
 		# job is to create a state describing the input sequence.
 		self.encoder_states = self.encoder_outputs_and_states[1:]
 
+#a dense layer to  resize the output states from the encoder to match the required shape of the input states to the decoder 
+    self.encoder_dense = keras.layers.Dense(self.config["target_sequence_length"],
+                                       activation='linear',
+                                       kernel_regularizer=self.config["kernel_regulariser"],
+                                       bias_regularizer=self.config["bias_regulariser"])
+
+    self.resized_encoder_states = self.encoder_dense(self.encoder_states)
+
+
 		# The decoder input will be set to zero (see random_sine function of the utils module).
 		# Do not worry about the input size being 1, I will explain that in the next cell.
 		self.decoder_inputs = keras.layers.Input(shape=(None, 1))
@@ -44,7 +53,7 @@ class SeqToSeqModel:
 
 		# Set the initial state of the decoder to be the ouput state of the encoder.
 		# This is the fundamental part of the encoder-decoder.
-		self.decoder_outputs_and_states = self.decoder(self.encoder_inputs, initial_state=self.encoder_states)
+		self.decoder_outputs_and_states = self.decoder(self.encoder_inputs, initial_state=self.resized_encoder_states)
 
 		# Only select the output of the decoder (not the states)
 		self.decoder_outputs = self.decoder_outputs_and_states[0]
