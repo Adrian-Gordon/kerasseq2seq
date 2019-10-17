4c2e633674d4c813ce9208660dcf71c33c3ac38c
Adrian Gordon <adriangordon60@gmail.com>
2019-10-17 12:29:23 +0100

    bfGenerator

--- generators/bfGenerator.py
+++ generators/bfGenerator.py
@@ -17,14 +17,34 @@ class BFGenerator:
   def generateTrainingSample(self,batch_size, source_sequence_length, offset, input_sequence_length, target_sequence_length):
 
     start_index = 0
-
+    within_sequence_offset = 0
+    input_batches =[]
+    output_batches=[]
 
     within_sequence_iterations = source_sequence_length - offset -input_sequence_length - target_sequence_length + 1
-    print(within_sequence_iterations)
+    #print(within_sequence_iterations)
     datafile_length = len(BFGenerator.x_train)
-    print(datafile_length)
+    #print(datafile_length)
 
-    
+    while True:
+      start_index, within_sequence_offset, input_sequence, output_sequence  = self.getNextSequence(start_index, offset, within_sequence_iterations, within_sequence_offset, input_sequence_length, target_sequence_length, source_sequence_length, datafile_length)
+
+      input_data = np.array(input_sequence[['layprice1','laydepth1','layprice2','laydepth2','layprice3','laydepth3','layprice4','laydepth4','layprice5','laydepth5','layprice6','laydepth6','layprice7','laydepth7','layprice8','laydepth8','layprice9','laydepth9','layprice10','laydepth10','backprice1','backdepth1','backprice2','backdepth2','backprice3','backdepth3','backprice4','backdepth4','backprice5','backdepth5','backprice6','backdepth6','backprice7','backdepth7','backprice8','backdepth8','backprice9','backdepth9','backprice10','backdepth10']])
+      input_batches.append(input_data)
+      output_data = np.array(output_sequence[['layprice1']])
+      output_batches.append(output_data)
+
+      if len(input_batches) == batch_size:
+        #print(input_batches)
+        #print(output_batches)
+        input_batches_array = np.array(input_batches)
+        output_batches_array = np.array(output_batches)
+        decoder_input_batches = np.zeros((batch_size, target_sequence_length, 1))
+        input_batches = []
+        output_batches = []
+        yield([input_batches_array, decoder_input_batches], np.array(output_batches_array))
+
+      #print(i, start_index, within_sequence_offset)
 
   def getNextSequence(self, start_index, dataset_offset, within_sequence_iterations, within_sequence_offset, input_sequence_length, target_sequence_length, source_sequence_length, datafile_length):
     '''
@@ -42,10 +62,12 @@ class BFGenerator:
 
     '''
     if within_sequence_offset == within_sequence_iterations :
+      #print("end of within sequence iterations")
       start_index = start_index + source_sequence_length
       within_sequence_offset = 0
 
     if(start_index >= datafile_length): #go back to the start
+        #print("Reached the end, go back to the start")
         start_index = 0
         within_sequence_offset = 0
        
@@ -55,11 +77,11 @@ class BFGenerator:
     #print(input_sequence_start_index )
     #print(input_sequence_end_index)
     input_sequence = BFGenerator.x_train[input_sequence_start_index:input_sequence_end_index]
-    print(input_sequence["layprice1"])
+    #print(input_sequence["layprice1"])
     output_sequence_start_index = input_sequence_end_index 
     output_sequence_end_index = output_sequence_start_index + target_sequence_length
     output_sequence = BFGenerator.x_train[output_sequence_start_index:output_sequence_end_index]
-    print(output_sequence["layprice1"])
+    #print(output_sequence["layprice1"])
    
     within_sequence_offset = within_sequence_offset + 1
     return start_index, within_sequence_offset, input_sequence, output_sequence
@@ -67,10 +89,19 @@ class BFGenerator:
 
 
 #test
-
-bfgenerator = BFGenerator("../data/testdata.csv",60, 30, 10, 5)
+'''
+bfgenerator = BFGenerator("/home/adrian/Development/bftimeseries/nodejs/data/preprocessed_generate.csv",60, 30, 10, 5)
 print(BFGenerator.n_sequences)
+print(BFGenerator.within_sequence_iterations)
 
-#bfgenerator.generateTrainingSample(1, 60, 0, 20, 10)
+the_generator = bfgenerator.generateTrainingSample(10, 60, 30, 10, 5)
+
+for i in range(200):
+  inputs, outputs = next(the_generator)
+#print(inputs[0].shape, inputs[1].shape, outputs.shape)
+  print(i,"Inputs:" ,inputs[0][0][0][0], "Outputs: ", outputs[0][0][0])
+
+
 #rval = bfgenerator.getNextSequence(540, 30, 16, 16, 10, 5, 60, 600)
-#print(rval)
+#print(rval
+'''
