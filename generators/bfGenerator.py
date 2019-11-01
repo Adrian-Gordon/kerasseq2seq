e569de5a1445e3632c35111492aa0b2513ffc3ed
Adrian Gordon <adrian@goraise.co.uk>
2019-11-01 15:53:27 +0000

    handle exceptions

--- generators/bfGenerator.py
+++ generators/bfGenerator.py
@@ -35,23 +35,30 @@ class BFGenerator:
       start_index, within_sequence_offset, input_sequence, output_sequence  = self.getNextSequence(start_index, offset, within_sequence_iterations, within_sequence_offset, input_sequence_length, target_sequence_length, source_sequence_length, datafile_length)
 
      # input_data = np.array(input_sequence[['layprice1','laydepth1','layprice2','laydepth2','layprice3','laydepth3','layprice4','laydepth4','layprice5','laydepth5','layprice6','laydepth6','layprice7','laydepth7','layprice8','laydepth8','layprice9','laydepth9','layprice10','laydepth10','backprice1','backdepth1','backprice2','backdepth2','backprice3','backdepth3','backprice4','backdepth4','backprice5','backdepth5','backprice6','backdepth6','backprice7','backdepth7','backprice8','backdepth8','backprice9','backdepth9','backprice10','backdepth10']])
-      input_data = self.normalise_to_first_value(np.array(input_sequence[input_attributes]))
-      input_batches.append(input_data)
-      output_data = self.normalise_to_first_value(np.array(output_sequence[output_attributes]))
-      output_batches.append(output_data)
-      if len(input_batches) == batch_size:
-        #print(input_batches)
-        #print(output_batches)
-        input_batches_array = np.array(input_batches)
-        output_batches_array = np.array(output_batches)
-        decoder_input_batches = np.zeros((batch_size, target_sequence_length, 1))
-        input_batches = []
-        output_batches = []
-        #print("size=%d start_index=%d within_sequqnce_offset %d", len(input_batches_array), start_index, within_sequence_offset)
-        #print('batch input shape: ', input_batches_array.shape)
-        yield([input_batches_array, decoder_input_batches], np.array(output_batches_array))
+      try:
 
-      #print(i, start_index, within_sequence_offset)
+        input_data = self.normalise_to_first_value(np.array(input_sequence[input_attributes]))
+        output_data = self.normalise_to_first_value(np.array(output_sequence[output_attributes]))
+      except:
+        pass
+      else:
+        input_batches.append(input_data)
+        output_batches.append(output_data)
+        #print(len(input_batches))
+        if len(input_batches) == batch_size:
+          #print(input_batches)
+          #print(output_batches)
+          input_batches_array = np.array(input_batches)
+          output_batches_array = np.array(output_batches)
+          decoder_input_batches = np.zeros((batch_size, target_sequence_length, 1))
+          input_batches = []
+          output_batches = []
+          #print("size=%d start_index=%d within_sequqnce_offset %d", len(input_batches_array), start_index, within_sequence_offset)
+          #print('batch input shape: ', input_batches_array.shape)
+          yield([input_batches_array, decoder_input_batches], np.array(output_batches_array))
+      #except:
+        #print("An exception")
+        #print(i, start_index, within_sequence_offset)
 
   def getNextSequence(self, start_index, dataset_offset, within_sequence_iterations, within_sequence_offset, input_sequence_length, target_sequence_length, source_sequence_length, datafile_length):
     '''
@@ -98,14 +105,10 @@ class BFGenerator:
     #print(input_data)
     #print(input_data.shape[1])
     normalised = []
-    try:
-      for col_i in range(input_data.shape[1]):
-        normalised_col = [((float(p) / float(input_data[0, col_i])) - 1) for p in input_data[:, col_i]]
-        normalised.append(normalised_col)
-      normalised = np.array(normalised).T
-    except:
-      print(input_data)
-    #print(normalised)
+    for col_i in range(input_data.shape[1]):
+      normalised_col = [((float(p) / float(input_data[0, col_i])) - 1) for p in input_data[:, col_i]]
+      normalised.append(normalised_col)
+    normalised = np.array(normalised).T
     return(normalised)
 
 if __name__ == '__main__':
@@ -113,14 +116,7 @@ if __name__ == '__main__':
   print(BFGenerator.n_sequences)
   print(BFGenerator.within_sequence_iterations)
 
-  the_generator = bfgenerator.generateTrainingSample(1, 60, 30,['layprice1','laydepth1','backprice1','backdepth1'],10,['layprice1','backprice1'],5)
+  the_generator = bfgenerator.generateTrainingSample(90000, 60, 30,['layprice1','laydepth1','backprice1','backdepth1'],10,['layprice1','backprice1'],5)
   inputs, outputs = next(the_generator)
-  print(inputs)
-  print(outputs)
-
-  ''' for i in range(1):
-    inputs, outputs = next(the_generator)
-    #print(inputs[0].shape, inputs[1].shape, outputs.shape)
-    print(i,"Inputs:" ,inputs)
-    print(i,"Outputs", outputs)
-  '''  
+  print(inputs[0].shape, inputs[1].shape, outputs.shape)
+  
