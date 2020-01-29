73f93163df831688d671329ac216ab305f1a2fe8
Adrian Gordon <adrian@goraise.co.uk>
2020-01-29 19:56:59 +0000

    added dockerfile

---
+++ dockerbuild.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile.inferenceserver -t inferenceserver:1.0
--- inferenceServer.py
+++ inferenceServer.py
@@ -4,14 +4,8 @@ import numpy as np
 
 from generators import BFGenerator
 
-config= {
-    "num_input_features": 4,
-    "num_output_features":2,
-    "input_sequence_length":10,
-    "target_sequence_length": 5,
-    "model_save_path":"./data/kerasmodel.h5"
-    
-}
+with open('./config/config.json') as json_file:
+    config = json.load(json_file)
 
 
 class InferenceServerHandler(BaseHTTPRequestHandler):
@@ -43,3 +37,4 @@ if __name__ == '__main__':
   server = HTTPServer(('',3002), InferenceServerHandler)
   print("listening on port 3002")
   server.serve_forever()
+
