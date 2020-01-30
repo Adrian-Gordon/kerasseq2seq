f7255a3ec8a55cfed6129ad012241f3166ef13d6
Adrian Gordon <adrian@goraise.co.uk>
2020-01-30 21:02:30 +0000

    load model

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile.inferenceserver -t inferenceserver:1.0
+docker build -f Dockerfile.inferenceserver -t inferenceserver:1.0 .
--- inferenceServer.py
+++ inferenceServer.py
@@ -2,10 +2,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import numpy as np 
 
+import keras
+from model import SeqToSeqModel
 from generators import BFGenerator
 
 with open('./config/config.json') as json_file:
     config = json.load(json_file)
+
+#LOAD AN EXISTING TRAINED MODEL
+model = SeqToSeqModel(config)
+model.load()
+print("MODEL SUMMARY: ")
+print(model.model.summary())
 
 
 class InferenceServerHandler(BaseHTTPRequestHandler):
@@ -28,8 +36,12 @@ class InferenceServerHandler(BaseHTTPRequestHandler):
 
     self.data = np.array(json.loads(self.data_string))
     prediction_input_data, decoder_input_data = BFGenerator.prepare_prediction_data(self.data, config['target_sequence_length'])
-    print(prediction_input_data)
-    print(decoder_input_data)
+    predicted_y=model.predict(prediction_input_data, decoder_input_data)
+    print(predicted_y)
+    post_processed_predictions = BFGenerator.process_prediction(self.data,[0,2], predicted_y[0])
+    print(post_processed_predictions)
+    self.wfile.write(json.dumps(post_processed_predictions.tolist()).encode("utf-8"))
+    #self.wfile.write(json.dumps(model.model.summary()).encode("utf-8"))
 
 
 
